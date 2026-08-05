d76c7fc64e1f5d055981229781eb1eaecc9115a9
buyath <buyaththayub@gmail.com>
2026-08-05 16:13:10 +0000

    feat: added pages routing and navbar

---
+++ src/pages/PlaceOrder.jsx
@@ -0,0 +1,8 @@
+const  PlaceOrder = () => {
+    return (
+        <div>
+            <h1>Place Order Page</h1>
+        </div>
+    )
+}
+export default PlaceOrder;
---
+++ src/pages/order.jsx
@@ -0,0 +1,8 @@
+const Orders = () => {
+    return (
+        <div>
+            <h1>Order Page</h1>
+        </div>
+    )
+}
+export default Orders;
